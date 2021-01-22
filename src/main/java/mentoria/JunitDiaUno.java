7b209fbd8f55c8e8cedbddddf0692bbebeebed77
fahelo.dev <chiletsoft@gmail.com>
2021-01-22 09:45:55 -0300

    repositorio listo para comenzar bc Junit 2021

--- src/main/java/mentoria/JunitDiaUno.java
+++ src/main/java/mentoria/JunitDiaUno.java
@@ -10,37 +10,7 @@ public class JunitDiaUno {
                 RES: 42581
      */
 
-    public JunitDiaUno(){
 
-    }
-
-    public int cuadradoDeDigito(int numero){
-
-        //1. transformar el numero a un String
-        int resultadoInt;
-        String resultadoString ="";
-        String arrayDigitos = Integer.toString(numero);
-
-        //2. Ciclo for para poder recorrer los caracteres del String y aplicar la funcionalidad
-        for (int i = 0; i < arrayDigitos.length(); i++) {
-            //obtengo el caracter-digito
-            char cabezal = arrayDigitos.charAt(i);
-            int obtenercuadradoDigito = Character.getNumericValue(cabezal);
-            obtenercuadradoDigito = obtenercuadradoDigito*obtenercuadradoDigito;
-            resultadoString = resultadoString+Integer.toString(obtenercuadradoDigito);
-        }
-        resultadoInt =Integer.parseInt(resultadoString);
-        return resultadoInt;
-    }
-
-    public static void main(String[] fae){
-
-        JunitDiaUno obj = new JunitDiaUno();
-
-        System.out.println("Resultado: "+obj.cuadradoDeDigito(259));
-        System.out.println("Resultado: "+obj.cuadradoDeDigito(111));
-        System.out.println("Resultado: "+obj.cuadradoDeDigito(999));
-    }
 
 
 }
